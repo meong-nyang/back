8c9e443da2b4a1d09a3c646a0ae9afc45b143366
gurwls980 <gurwls980@gmail.com>
2024-10-11 17:48:09 +0900

    상품 entity추가

--- src/main/java/com/meongnyang/shop/entity/Product.java
+++ src/main/java/com/meongnyang/shop/entity/Product.java
@@ -1,4 +1,29 @@
 package com.meongnyang.shop.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    private Long id;
+    private String productName;
+    private Long petGroupId;
+    private Long categoryId;
+    private Long productPrice;
+    private Long sellPrice;
+    private Long discountPrice;
+    private String productDetail;
+    private String productBrand;
+    private String productModel;
+    private String productMemo;
+    private LocalDateTime productCreateDate;
+    private LocalDateTime productUpdateDate;
+    private String recommendation;
 }
